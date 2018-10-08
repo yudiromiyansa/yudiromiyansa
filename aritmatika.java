b00896233463da26e3b4a54ed38ab45bc38a2da4
Yudi Romiyansa <38884880+yudiromiyansa@users.noreply.github.com>
2018-10-08 10:05:18 +0700

    aritmatika.java
    pembelajaran aritmatika

---
+++ aritmatika.java
@@ -0,0 +1,60 @@
+import java.util.Scanner;
+
+public class OperatorAritmatika {
+
+    public static void main(String[] args) {
+        int angka1;
+        int angka2;
+        int hasil;
+
+        Scanner keyboard = new Scanner(System.in);
+
+        System.out.print("Input angka-1: ");
+        angka1 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        angka2 = keyboard.nextInt();
+
+        // penjumlahan
+        hasil = angka1 + angka2;
+        System.out.println("Hasil = " + hasil);
+
+        System.out.print("Input angka-1: ");
+        angka1 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        angka2 = keyboard.nextInt();
+
+        // pengurangan
+        hasil = angka1 - angka2;
+        System.out.println("Hasil = " + hasil);
+
+         System.out.print("Input angka-1: ");
+        angka1 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        angka2 = keyboard.nextInt();
+
+        // perkalian
+        hasil = angka1 * angka2;
+        System.out.println("Hasil = " + hasil);
+
+
+        System.out.print("Input angka-1: ");
+        angka1 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        angka2 = keyboard.nextInt();
+
+        // Pembagian
+        hasil = angka1 / angka2;
+        System.out.println("Hasil = " + hasil);
+
+        System.out.print("Input angka-1: ");
+        angka1 = keyboard.nextInt();
+        System.out.print("Input angka-2: ");
+        angka2 = keyboard.nextInt();
+
+        // Sisa Bagi
+        hasil = angka1 % angka2;
+        System.out.println("Hasil = " + hasil);
+
+    }
+
+}
