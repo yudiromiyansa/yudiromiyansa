e32172a656541d7317f476f3cf6c99462171075c
Yudi Romiyansa <38884880+yudiromiyansa@users.noreply.github.com>
2018-10-08 10:30:41 +0700

    penugasan.java

---
+++ penugasan.java
@@ -0,0 +1,38 @@
+public class OperatorPenugasan {
+
+    public static void main(String[] args) {
+        int a;
+        int b;
+
+        // Pengisian nilai
+        a = 5;
+        b = 10;
+
+        // penambahan
+        b += a;
+        // sekarang b = 15
+        System.out.println("Penambahan : " + b);
+
+        // pengurangan
+        b -= a;
+        // sekarang b = 10 (karena 15-5)
+        System.out.println("Pengurangan : " + b);
+
+        // perkalian
+        b *= a;
+        // sekarang b = 50 (karena 10*5)
+        System.out.println("Perkalian : " + b);
+
+        // Pembagian
+        b /= a;
+        // sekarang b=10
+        System.out.println("Pembagian : " + b);
+
+        // Sisa bagi
+        b %= a;
+        // sekarang b=0
+        System.out.println("Sisa Bagi: " + b);
+
+    }
+
+}
